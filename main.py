631fbd3d1dff381e730cd6f5fa31acc3f5001d74
Loïc Poincelet <loic.poincelet@jdev.fr>
2025-04-17 17:07:30 +0200

    ajout de la suppression des documents

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, UploadFile, File, Form
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException
 from pathlib import Path
 from typing import List
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from  config import Config
+from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
 app.add_middleware(
@@ -43,3 +45,15 @@ async def list_files(chantier_id: str):
     
     files = [f.name for f in target_dir.iterdir() if f.is_file()]
     return {"files": files}
+
+@app.delete("/deletefile/")
+async def delete_file(chantier_id: str, filename: str):
+    file_path = UPLOAD_DIR / chantier_id / filename
+    
+    if file_path.exists() and file_path.is_file():
+        os.remove(file_path)
+        return {"message": f"File '{filename}' deleted successfully"}
+    else:
+        raise HTTPException(status_code=404, detail=f"File '{filename}' not found")
+
+app.mount("/files", StaticFiles(directory=UPLOAD_DIR), name="files")
